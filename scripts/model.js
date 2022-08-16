38d5c784c87e4f419157df0403463258feac9593
priyanshu <priyanshukumar61002@gmail.com>
2022-08-16 12:52:33 -0700

    added toFixed for 2 decimal places printing

--- scripts/model.js
+++ scripts/model.js
@@ -2,29 +2,29 @@
 const salaryOperations = {
     basicSalary : 0,
     getHra() {
-        return this.basicSalary * 0.20;
+        return this.basicSalary * 0.20.toFixed(2);
     },
     getDa() {
-        return this.basicSalary * 0.10;
+        return this.basicSalary * 0.10.toFixed(2);
     },
     getTa() {
-        return this.basicSalary * 0.20;
+        return this.basicSalary * 0.20.toFixed(2);
     },
     getPf() {
-        return this.basicSalary * 0.30;
+        return this.basicSalary * 0.30.toFixed(2);
     },
     getMa() {
-        return this.basicSalary * 0.40;
+        return this.basicSalary * 0.40.toFixed(2);
     },
     getTax() {
         if (this.basicSalary < 500000) {
-            return this.basicSalary * 0.05;
+            return this.basicSalary * 0.05.toFixed(2);
         }
         else if (this.basicSalary >= 500000 && this.basicSalary < 700000) {
-            return this.basicSalary * 0.10;
+            return this.basicSalary * 0.10.toFixed(2);
         }
         else {
-            return this.basicSalary * 0.20;
+            return this.basicSalary * 0.20.toFixed(2);
         }
     },
     getGrossSalary() {
