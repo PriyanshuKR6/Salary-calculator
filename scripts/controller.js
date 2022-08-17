a32e9fc66126d729041e45aed39fcc770c53899f
priyanshu <priyanshukumar61002@gmail.com>
2022-08-16 20:05:55 -0700

    "removed unnecessary comments"

--- scripts/controller.js
+++ scripts/controller.js
@@ -17,16 +17,6 @@ function compute() {
             appendChild(createPTag(key, salaryOperations[key]()));
     }
 }
-// function display() {
-//     document.getElementById('hra').innerText = hra;
-//     document.getElementById('da').innerText = da;
-//     document.getElementById('ta').innerText = ta;
-//     document.getElementById('pf').innerText = pf;
-//     document.getElementById('ma').innerText = ma;
-//     document.getElementById('tax').innerText = tax;
-//     document.getElementById('gross').innerText = grossSalary;
-//     document.getElementById('net').innerText = netSalary;
-// }
 
 function createPTag(key, val) {
     const pTag = document.createElement('p');
